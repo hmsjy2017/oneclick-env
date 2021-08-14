1f0150d53bdf0f0b38297533e5dd7147a5a4bb77
Tony <1462668901@qq.com>
2021-08-14 18:06:11 +0800

    Create oneclick-env.py

--- oneclick-env.py
+++ oneclick-env.py
@@ -36,12 +36,12 @@ def change_mirrors():
     print('\n正在备份原有 APT 源')
     os.system('sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak')
     os.system('sudo cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak')
-    print('\n备份完成。正在更换 APT 源为 SJTU 源')
+    print('\n正在更换 APT 源为 SJTU 源')
     os.system('sudo sed -i "s|http://raspbian.raspberrypi.org/raspbian/|https://mirrors.sjtug.sjtu.edu.cn/raspbian/raspbian/|g" /etc/apt/sources.list')
     os.system('sudo sed -i "s|http://deb.debian.org/debian|https://mirrors.sjtug.sjtu.edu.cn/debian|g" /etc/apt/sources.list')
     os.system('sudo sed -i "s|http://archive.raspberrypi.org/debian/|https://mirrors.sjtug.sjtu.edu.cn/raspberrypi/debian/|g" /etc/apt/sources.list.d/raspi.list')
     os.system('sudo apt update')
-    print('\n完成')
+    print('\n完成！')
 
 # 2.添加常用软件源
 def add_common_apt_repositories():
@@ -50,7 +50,6 @@ def add_common_apt_repositories():
     os.system('wget -qO- https://mirror.sjtu.edu.cn/raspbian-addons/KEY.gpg | sudo apt-key add -')
     os.system('echo "deb https://mirror.sjtu.edu.cn/raspbian-addons/debian/ buster main" | sudo tee /etc/apt/sources.list.d/raspbian-addons.list')
     os.system('sudo apt update')
-    print('\nRaspbian Addons 源添加成功')
     print('正在添加 Deb Multimedia 源')
     os.system('echo "deb http://mirrors.ustc.edu.cn/deb-multimedia/ buster main non-free" >> /etc/apt/sources.list')
     os.system('echo "# deb-src http://mirrors.ustc.edu.cn/deb-multimedia/ buster main non-free" >> /etc/apt/sources.list')
@@ -60,7 +59,7 @@ def add_common_apt_repositories():
     os.system('sudo dpkg -i deb-multimedia-keyring_2016.8.1_all.deb*')
     os.system('sudo apt update')
     os.system('rm deb-multimedia-keyring_2016.8.1_all.deb*')
-    print('\nDeb Multimedia 源添加成功')
+    print('\n添加完成！')
 
 # 3.安装 NodeJS	
 def install_nodejs():
@@ -71,14 +70,13 @@ def install_nodejs():
     os.system('sudo bash n stable')
     print('\n安装成功。正在更换 npm 源为淘宝源')
     os.system('npm config set registry https://registry.npm.taobao.org')
+    print('\n   NodeJS 安装成功')
     print('\nnode 版本：')
     os.system('node -v')
     print('\nnpm 版本：')
     os.system('npm -v')
     print('\nnpm 源：')
     os.system('npm config get registry')
-    print('\n   NodeJS 安装成功')
-    print('\n    祝您使用愉快\n')
 
 # 4.卸载 NodeJS
 def uninstall_nodejs():
@@ -111,8 +109,9 @@ def install_golang():
         os.system('export "PATH=$PATH:/usr/local/go/bin"')
         print('\n解压完成。正在删除已下载的压缩包')
         os.system('rm go1.16.7.linux-armv6l.tar.gz')  
-    os.system('go version')
-    print('\n   Golang 安装成功!')
+        print('\n   Golang 安装成功！')
+        print('\n   Golang 版本：')
+        os.system('go version')
 
 # 6.卸载 Golang
 def uninstall_golang():
@@ -128,8 +127,9 @@ def install_jdk():
     os.system('sudo apt-get update')
     print('\n正在安装 AdoptOpenJDK 16，请耐心等待')
     os.system('sudo apt install -y adoptopenjdk-16-hotspot')
+    print('\n   JDK 安装成功！')
+    print('\n   JDK 版本')
     os.system('java --version')
-    print('\n   JDK 安装成功')
 
 # 8.卸载 JDK
 def uninstall_jdk():
@@ -140,25 +140,30 @@ def uninstall_jdk():
 # 9.安装 Rust
 def install_rust():
     print('\n正在安装 Rust，请耐心等待')
-    os.system('wget https://raw.fastgit.org/hmsjy2017/oneclick-env/main/rustup-init.sh')
-    #os.system('wget https://cdn.jsdelivr.net/gh/rust-lang-nursery/rustup.rs/rustup-init.sh')
-    os.system('echo export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup >> ~/.bash_profile')
+    os.system('export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static')
+    os.system('export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup')
+    os.system('wget https://cdn.jsdelivr.net/gh/rust-lang-nursery/rustup.rs/rustup-init.sh')
     os.system('chmod +x rustup-init.sh')
     os.system('./rustup-init.sh -y')
     os.system('source $HOME/.cargo/env')
-    os.system('cargo --version')
     print('\n正在安装 Cargo 镜像')
     os.system('curl -sSf https://raw.fastgit.org/hmsjy2017/scripts/main/cargo.sh | sh')
     print('\n正在安装 Rustup 镜像')
-    os.system('echo "export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup" >> ~/.bash_profile')
-    os.system('rustup --version')
+    os.system('echo "RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static"  >> ~/.cargo/env')
+    os.system('rm rustup-init.sh*')
     print('\n   Rust 安装成功')
+    print('\n   Cargo 版本：')
+    os.system('cargo --version')
+    print('\n   rustup 版本：')
+    os.system('rustup --version')
+    print('\n   rustc 版本：')
+    os.system('rustc --version')
 
 # 10.卸载 Rust
 def uninstall_rust():
     print('\n正在卸载 Rust，请耐心等待')
     os.system('rustup self uninstall -y')
-    print('\n卸载完成!')
+    print('\n卸载完成！')
 
 # 11.安装 Ruby
 def install_ruby():
@@ -167,17 +172,18 @@ def install_ruby():
     os.system('\curl -sSL https://cdn.jsdelivr.net/gh/rvm/rvm@master/binscripts/rvm-installer | bash -s stable')
     os.system('source ~/.bashrc')
     os.system('source ~/.bash_profile')
-    os.system('rvm -v')
-    print('\nRVM 安装完成')
     print('\n正在安装 Ruby，请耐心等待')
     os.system('echo "ruby_url=https://cache.ruby-china.com/pub/ruby" > ~/.rvm/user/db')
     os.system('rvm install 3.0 --disable-binary')
     #可选 os.system('rvm docs generate-ri')
-    os.system('ruby -v')
     print('\n正在安装 gems 镜像')
     os.system('gem sources --add https://mirrors.tuna.tsinghua.edu.cn/rubygems/ --remove https://rubygems.org/')
     os.system('gem sources -l')
-    print('\n   Ruby 安装成功')
+    print('\n   Ruby 安装成功！')
+    print('\n   Ruby 版本：')
+    os.system('ruby -v')
+    print('\n   RVM 版本：')
+    os.system('rvm -v')
 
 # 12.卸载 Ruby
 def uninstall_ruby():
@@ -198,11 +204,12 @@ def install_docker():
         os.system('echo "deb [arch=armhf] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list')
     os.system('sudo apt-get update')
     os.system('sudo apt-get install -y docker-ce')
-    os.system('docker -v')
     print('\n正在配置 Docker 镜像站（DaoCloud）')
     os.system('curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io')
     os.system('sudo service docker restart')
-    print('\n   Docker 安装成功')
+    print('\n   Docker 安装成功！')
+    os.system('docker -v')
+    print('\n   Docker 版本：')
 
 # 14.卸载 Docker
 def uninstall_docker():
