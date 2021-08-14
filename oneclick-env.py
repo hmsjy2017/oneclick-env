9917601aa2d6f8c14519494066df7513126edbe2
Tony <1462668901@qq.com>
2021-08-14 18:52:54 +0800

    fix

--- oneclick-env.py
+++ oneclick-env.py
@@ -202,9 +202,15 @@ def install_docker():
     os.system('sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common')
     os.system('curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -')
     if bit == '64':
-        os.system('echo "deb [arch=arm64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list')
+        os.system('wget https://raw.fastgit.org/hmsjy2017/scripts/main/docker-64.sh')
+        os.system('chmod +x docker-64.sh')
+        os.system('bash docker-64.sh')
+        os.system('rm docker-64.sh')
     elif bit == '32':
-        os.system('echo "deb [arch=armhf] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list')
+        os.system('wget https://raw.fastgit.org/hmsjy2017/scripts/main/docker-32.sh')
+        os.system('chmod +x docker-32.sh')
+        os.system('bash docker-32.sh')
+        os.system('rm docker-32.sh')
     os.system('sudo apt-get update')
     os.system('sudo apt-get install -y docker-ce')
     print('\n正在配置 Docker 镜像站（DaoCloud）')
